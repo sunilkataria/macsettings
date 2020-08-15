ac1be4ecd706b5eeb44e0202856b41e2008b38e4
Sunil Kataria <sunil_kataria@outlook.cin>
2020-08-15 20:03:53 +0530

    fix dirs

--- list_zoc_files.sh
+++ list_zoc_files.sh
@@ -1,9 +1,10 @@
-ls -ld ./Library/Application\ Support/EmTec
-ls -ld ./Library/Preferences/com.emtec.registry.plist
-ls -ld ./Library/Preferences/com.emtec.ct.nonfixedfonts.txt
-ls -ld ./Library/Preferences/com.emtec.zoc7.plist
+ls -ld ~/Library/Application\ Support/EmTec
+ls -ld ~/Library/ApplicationSupport/EmTec
+ls -ld ~/Library/Preferences/com.emtec.registry.plist
+ls -ld ~/Library/Preferences/com.emtec.ct.nonfixedfonts.txt
+ls -ld ~/Library/Preferences/com.emtec.zoc7.plist
 
-ls -ld ./Library/Application\ Support/ZOC7*
-ls -ld ./Library/Preferences/com.emtec.zoc7.plist
-ls -ld ./Library/Logs/ZOC*
-ls -ld ./Library/Caches/com.apple.helpd/SDMHelpData/Other/English/HelpSDMIndexFile/ZOC*
+ls -ld ~/Library/Application\ Support/ZOC7*
+ls -ld ~/Library/Preferences/com.emtec.zoc7.plist
+ls -ld ~/Library/Logs/ZOC*
+ls -ld ~/Library/Caches/com.apple.helpd/SDMHelpData/Other/English/HelpSDMIndexFile/ZOC*
